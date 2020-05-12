6bf259ce2f1d1628a95144336a0a664a58ec43bb
cool8676 <cool8676@gmail.com>
2020-05-13 01:04:29 +0800

    createCard by AJAX and Vue

--- app/javascript/components/list.vue
+++ app/javascript/components/list.vue
@@ -2,18 +2,54 @@
   <div class="list">
     <h2 class="header">{{ list.name }}</h2>
     <div class="deck">
-      <Card v-for="card in list.cards" :card="card" :key="card.id"></Card>
+      <Card v-for="card in cards" :card="card" :key="card.id"></Card>
+
+      <div class="input-area">
+        <textarea class="content" id="" v-model="content"></textarea>
+        <button class="button" @click="createCard">新增卡片</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Rails from '@rails/ujs';
 import Card from 'components/card';
 
 export default {
   name: 'List',
   props: ["list"],
-  components: { Card }
+  components: { Card },
+  data: function() {
+    return {
+      content: '',
+      cards: this.list.cards
+    }
+  },
+  methods: {
+    createCard(event) {
+      event.preventDefault();
+      
+      let data = new FormData();
+      data.append("card[name]", this.content);
+      data.append("card[list_id]", this.list.id);
+
+      Rails.ajax({
+        url: '/cards',
+        type: 'POST',
+        data,
+        dataType: 'json',
+        success: resp => {
+         this.content = '',
+         this.cards.push(resp);
+        },
+        error: err => {
+          console.log(err);
+          this.cards
+        }
+      });
+    }
+  }
 }
 </script>
 
@@ -28,6 +64,26 @@ export default {
     .deck {
       @apply .mt-2;
 
+    }
+
+    .input-area {
+      @apply .mt-3;
+      
+      .content {
+        @apply .w-full .p-2 rounded-sm;
+
+        &:focus {
+          @apply .outline-none;
+        }
+      }
+
+      .button {
+        @apply .px-3 .py-1 .font-semibold .text-sm .bg-blue-300 rounded;
+
+        &:focus {
+          @apply .outline-none;
+        }
+      }
     }
   }
 </style>
