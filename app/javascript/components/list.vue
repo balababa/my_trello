369b27338d490635b1b6d61defe499786c859f24
cool8676 <cool8676@gmail.com>
2020-05-14 02:38:04 +0800

    Toggle switch add card button

--- app/javascript/components/list.vue
+++ app/javascript/components/list.vue
@@ -2,11 +2,18 @@
   <div class="list">
     <h2 class="header">{{ list.name }}</h2>
     <div class="deck">
-      <Card v-for="card in cards" :card="card" :key="card.id"></Card>
+      <Card class="shadow-md rounded" v-for="card in cards" :card="card" :key="card.id"></Card>
 
       <div class="input-area">
-        <textarea class="content" id="" v-model="content"></textarea>
-        <button class="button" @click="createCard">新增卡片</button>
+        
+       
+        <button v-if="!editing" class="addbutton" @click="newCard"><span><i  class="fas fa-plus" ></i>&nbsp</span>新增卡片</button>
+
+ 
+        
+        <textarea v-if="editing" class="content" id="" v-model="content"></textarea>
+        <button v-if="editing" class="button" @click="createCard">建立卡片</button>
+        <button v-if="editing" class="cancel" @click="cancelNew"><span ><i  class="fas fa-times"></i></span></button>
       </div>
     </div>
   </div>
@@ -23,7 +30,8 @@ export default {
   data: function() {
     return {
       content: '',
-      cards: this.list.cards
+      cards: this.list.cards,
+      editing: false
     }
   },
   methods: {
@@ -42,12 +50,22 @@ export default {
         success: resp => {
          this.content = '',
          this.cards.push(resp);
+         this.editing = false;
         },
         error: err => {
           console.log(err);
           this.cards
         }
       });
+    },
+    newCard(event) {
+      event.preventDefault();
+      this.editing = true;
+
+          },
+    cancelNew(event) {
+      event.preventDefault();
+      this.editing = false;
     }
   }
 }
@@ -70,18 +88,36 @@ export default {
       @apply .mt-3;
       
       .content {
-        @apply .w-full .p-2 rounded-sm;
+        @apply .w-full .p-2 rounded .shadow-md mb-2;
 
         &:focus {
           @apply .outline-none;
         }
       }
 
+      .addbutton{
+        @apply .text-gray-600 rounded px-3 py-1 w-full text-left;
+
+        &:hover {
+          @apply .bg-gray-400;
+        }
+      }
+
+  
+
       .button {
-        @apply .px-3 .py-1 .font-semibold .text-sm .bg-blue-300 rounded;
+        @apply .px-3 .py-2 .font-semibold .text-sm .bg-green-400 rounded;
 
         &:focus {
           @apply .outline-none;
+        }
+      }
+
+      .cancel {
+        @apply .ml-2 .text-2xl .text-gray-600 .align-middle; 
+
+        &:hover {
+          @apply .text-black;
         }
       }
     }
