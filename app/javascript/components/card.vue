53bf7d76ca6f8696816799e4c04211cc5d6c4b3f
cool8676 <cool8676@gmail.com>
2020-05-20 00:19:56 +0800

    Edit card

--- app/javascript/components/card.vue
+++ app/javascript/components/card.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="card">
+<div>
+    <div class="card" @click="editing=true">
     {{ card.name }}
   </div>
+
+  <div v-if="editing" class="dialog" >
+    <div class="inner">
+      <a class="closeButton" @click="close"><i class="fas fa-times" ></i></a>
+      <textarea class="content" v-model="content"></textarea>
+      <button class="updateButton" @click="update">更新</button>
+    </div>
+  </div>
+</div>
+
 </template>
 
 <script>
 export default {
-  name: 'Card',
-  props: ["card"]
+  nam: 'Card',
+  data: function() {
+    return {
+      editing: false,
+      content: this.card.name
+    }
+  },
+  props: ["card"],
+  methods: {
+    close(event) {
+      event.preventDefault();
+      this.editing = false;
+      this.content = this.card.name
+    },
+
+    update(event) {
+      event.preventDefault();
+      this.$store.dispatch("updateCard", { id: this.card.id, name: this.content });
+      this.editing = false;
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   .card {
-    @apply .bg-gray-100 .mb-2 .rounded-sm px-2 py-2;
+    @apply .bg-gray-100 .mb-2 .rounded-sm .px-2 .py-2 .cursor-pointer;
+  }
+
+  .dialog {
+    background-color: rgba(0,0,0,0.7);
+    @apply .fixed .w-full .h-full .top-0 .left-0 .flex .items-center .justify-center;  
+
+    .inner {
+      @apply .bg-white .flex .flex-col .px-6 .py-2 .w-1/2;
+      .closeButton {
+        @apply .text-gray-800 .text-right .cursor-pointer;
+      }
+
+      .content {
+        @apply .px-2 .py-2 .border-2 .border-gray-300 .mt-2;
+
+        &:focus {
+          @apply outline-none;
+        }
+      }
+
+      .updateButton {
+        @apply .bg-gray-400 .px-3 .py-2 .mt-2 .rounded;
+        &:focus {
+          @apply outline-none;
+        }
+      }
+    }
   }
 </style>
--- app/javascript/stores/list.js
+++ app/javascript/stores/list.js
@@ -16,10 +16,34 @@ export default new Vuex.Store({
   mutations: {
     UPDATE_LISTS(state, lists) {
         state.lists = lists;
+    },
+    
+    REPLACE_CARD(state, card) {
+      let list_index = state.lists.findIndex(list => list.id == card.list_id);
+      let card_index = state.lists[list_index].cards.findIndex(item => item.id == card.id);
+      console.log(state.lists[list_index].cards[card_index]);
+      state.lists[list_index].cards.splice(card_index, 1, card);
     }
   },
 
   actions: {
+    updateCard( { commit }, { id, name }) {
+      let data = new FormData();
+      data.append("card[name]", name);
+
+      Rails.ajax({
+        url: `/cards/${id}`,
+        type: 'PUT',
+        data,
+        dataType: 'json',
+        success: resp => {
+          commit("REPLACE_CARD", resp)
+        },
+        error: err => {
+          console.log(err)
+        }
+      })
+    },
 
     moveList( { commit, state }, event) {
       let data = new FormData();
